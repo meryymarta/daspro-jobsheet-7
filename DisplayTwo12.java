e9502264cfd0cc63d6b0bc39197f744a726ea5f9
meryymarta <meriammartadinata@gmail.com>
2024-11-01 15:06:03 +0700

    push to github

--- DisplayTwo12.java
+++ DisplayTwo12.java
@@ -12,7 +12,7 @@ public class DisplayTwo12 {
     for(int i =1; i<= numInput; i++) {
         if (i % 2 == 0) {
        
-            System.out.print( i + " ");
+            System.out.print(i + " ");
 
         }
     }
