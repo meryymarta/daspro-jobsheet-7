8a8e7fcdbf5e8ba27ad3cf039e3f0c50c2a9aceb
meryymarta <meriammartadinata@gmail.com>
2024-11-01 15:10:34 +0700

    push to github

--- WhileMultiple12.java
+++ WhileMultiple12.java
@@ -1,29 +1,31 @@
-
 import java.util.Scanner;
-public class ForMutiples12 {
+public class WhileMultiple12 {
     public static void main(String[] args) {
         Scanner input12 = new Scanner(System.in);
 
-        //declaration variable
-        int multiple, sum = 0, counter = 0;
-        double average = 0;
+      //Declaration Variable
+      int multiple, sum = 0, counter = 0;
+      double average = 0;
 
-        System.out.print(" Input the multiple: ");
-        multiple = input12.nextInt();
+      System.out.print(" Input the multiple: ");
+      multiple = input12.nextInt();
 
-        for(int i = 1; i <= 50; i++) {
-            if (i % multiple == 0) {
-                sum = sum + i;
-                counter++;
-            }
+      int i = 1;
+      while (i <= 50) {
+        if (i % multiple == 0) {
+            sum = sum + i;
+            counter++;
         }
-        average = sum / counter;
+        i++;
+      }
+      average = sum / counter;
     
     System.out.printf(" There are %d number that are multiple of %d in range 1 to 50.\n", counter, multiple);
     System.out.printf(" The sum of all mulptiples of %d in range 1 to 50 is %d. \n", multiple, sum);
     System.out.printf(" The average of all multiples of %d in range 1 to 50 is %.2f. \n ", multiple, average);
-    
-    }
- 
 }
+
+    }
+     
+    
 
