5fa262348095a13c993efec04086a13f760f4033
meryymarta <meriammartadinata@gmail.com>
2024-11-01 10:44:54 +0700

    push to github

---
+++ TheTriangle12.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class TheTriangle12 {
+    public static void main(String[] args) {
+        Scanner input12 = new Scanner(System.in);
+
+        //Declaration variable
+        int numInput, i = 0;
+        String s = " ";
+
+        System.out.print("Input some number: ");
+        numInput = input12.nextInt();
+        
+        while (i < numInput) {
+            s += " *";
+            System.out.println(s);
+            i++;
+        
+        
+        }
+        
+
+    } }
+    
