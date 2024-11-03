c497859dff9ee6cd9898a3402f92e5b4dd101a62
meryymarta <meriammartadinata@gmail.com>
2024-11-03 19:11:07 +0700

    push to github

--- TheTriangle12.java
+++ TheTriangle12.java
@@ -4,18 +4,19 @@ public class TheTriangle12 {
         Scanner input12 = new Scanner(System.in);
 
         //Declaration variable
-        int numInput, i = 0;
+        int numInput, i = 0, j = 0;
         String s = " ";
 
         System.out.print("Input some number: ");
         numInput = input12.nextInt();
         
-        for ( i = 0; i < numInput; i++) {
-            s += " *";
-            System.out.println(s);
+        for ( i = 0; i < numInput; i++){
+            for  (j = 0; j <= i; j++) {
+              s += " *";
         
         }
-        
+        System.out.println(s);
+        s = " ";
 
-    } }
+    } }}
     
